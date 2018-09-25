67c6184215d4c1d04c7d00b0f7be511f2cd42b38
Jade Allen Cook <jadeallencook@gmail.com>
2018-09-24 19:43:40 -0600

    🛠 Updated the loading page

--- src/modules/pages/loading.js
+++ src/modules/pages/loading.js
@@ -1,5 +1,8 @@
 module.exports = {
-    load: function() {
-        console.log(true);
+    load: function(page) {
+        const root = document.getElementById('root');
+        let elem = document.createElement('h1');
+        elem.innerText = 'Loading...';
+        root.appendChild(elem);
     }
 }
